beec34d8c2dffacd046eb634286a709b4b4cb717
Shushanna <shushannagrigoryan7@gmail.com>
2022-08-22 19:32:09 +0400

    6

--- src/main/java/school/mjc/stage0/loops/task4/FixMe.java
+++ src/main/java/school/mjc/stage0/loops/task4/FixMe.java
@@ -2,7 +2,6 @@ package school.mjc.stage0.loops.task4;
 
 public class FixMe {
     public static void main(String[] args) {
-        System.out.println("ERROR");
         for (int i = 0; i < 100; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,10 +2,9 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-        System.out.println("ERROR");
         if(numberToGoUntil < toBreakWith){
             System.out.println("iterating till the end");
-            for(int i =1; i<toBreakWith;i++){
+            for(int i =1; i<=toBreakWith;i++){
                 System.out.println(i);
             }
         }
